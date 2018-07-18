90c455d6bc2c11f7a874f58e08931db4ecc68a35
casey van-roose <kcvanroose@gmail.com>
2018-07-18 18:12:08 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array_of_words = self.split(/\?|\.|!/)
+    array_of_words.delete_if{|e| e.length == 0}.count
   end
+  
 end
